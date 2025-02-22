65e99e2bac6623ad94ffba72e114cc6859d15065
jaymehta2189 <jaymehta249@gmail.com>
2025-02-22 23:52:03 +0530

    auth screen

--- Duhacksfrontend/app/_layout.jsx
+++ Duhacksfrontend/app/_layout.jsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <>
+    
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="auth" />
         <Stack.Screen name="(app)" />
